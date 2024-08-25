518562835971b9bfb8f53c4728c687c9387ae45a
bikramia <bikram.rhce@gmail.com>
2024-08-25 17:46:49 -0400

    TypeScript-section2-Enums

---
+++ Section2-Fundamentals/src/enums.ts
@@ -0,0 +1,10 @@
+// one way
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+
+// Pascalcase convention - first letter of every word is uppercase
+//Enum 
+const enum Size { Small = 1, Medium, Large }; 
+let mySize: Size = Size.Medium;
+console.log(mySize);
