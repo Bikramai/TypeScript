51b3ad6f4b10d7a11aa48313951b9d723a31c093
bikramia <bikram.rhce@gmail.com>
2024-08-25 14:11:02 -0400

    TypeScript-section2-built-in Type -Primitive -put in separatefile

---
+++ Section2-Fundamentals/src/primitive.ts
@@ -0,0 +1,16 @@
+let sales = 123_456_789;
+let course = 'TypeScript';
+let is_publish = true;
+let level;
+
+/* 
+In TypeScript, if we have a large number, we can separate these digits using an underscore.
+In TypeScript, we don't always have to annotate our variables, because the typescript 
+compiler can infer or detect the type of our variable based on a value. We don't have to annotate.
+so let remove the annotation from each variables.
+
+For example, because we have initialized this variables to a number, string, boolean. 
+TypeScript compiler knows that these variables. So, we don't have to annotate it. 
+If we remove the annotation, and hover our mouse over these variables look, we can
+see that sales is number, course is string, and is_publish is boolean.
+*/
