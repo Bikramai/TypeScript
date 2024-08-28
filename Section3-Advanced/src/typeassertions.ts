f6ce3c4f1924ee981cf390e4c188ffee5c27dffa
bikramia <bikram.rhce@gmail.com>
2024-08-28 18:13:28 -0400

    Type Assertions

---
+++ Section3-Advanced/src/typeassertions.ts
@@ -0,0 +1,9 @@
+// one way of Assertion
+let phone = document.getElementById('phone') as HTMLInputElement;
+
+// another way of Assertion
+// let phone = <HTMLInputElemen>document.getElementById('phone');
+
+// HTMLElement
+// HTMLInputElement
+phone.value
