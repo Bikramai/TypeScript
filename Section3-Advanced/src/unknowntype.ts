714d018c5ee5c5ca2a0dd42c94b9808963e591b1
bikramia <bikram.rhce@gmail.com>
2024-08-28 19:32:54 -0400

    Unknown Type

---
+++ Section3-Advanced/src/unknowntype.ts
@@ -0,0 +1,8 @@
+// function render(document: unknown) {
+    //Narrowing
+//     if (document.toUpperCase()) {
+// }
+//     document.move();
+//     document.fly();
+//     document.wahtever();
+// }
