9f4c43474dfae42340becf367f0d975496c9833f
bikramia <bikram.rhce@gmail.com>
2024-09-09 21:59:44 -0400

    Generic constraints

---
+++ Section5-Generics/genericconstraints.ts
@@ -0,0 +1,7 @@
+// function echo<Textends number | string>(value: T) {}
+ 
+//  // Restrict using a shape object
+//  function echo<Textends { name: string }>(value: T) {}
+ 
+//  // Restrict using an interface or a class
+//  functionecho<Textends Person>(value: T) {}
