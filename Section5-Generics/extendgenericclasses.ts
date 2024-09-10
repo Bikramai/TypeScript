13551abd46c2fb7ec6758dfad941bca4182fcfee
bikramia <bikram.rhce@gmail.com>
2024-09-09 22:03:39 -0400

    Extends generic classes

---
+++ Section5-Generics/extendgenericclasses.ts
@@ -0,0 +1,8 @@
+// // Passing on generic type parameters 
+// class CompressibleStore<T> extends Store<T> { }
+
+// // Constraining generic type parameters 
+// class SearchableStore<Textends { name:string }> extendsStore<T> { }
+
+// // Fixing generic type parameters 
+// class ProductStore extends Store<Product> { }
