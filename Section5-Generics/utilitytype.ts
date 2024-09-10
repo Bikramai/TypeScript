5dee8c9d9efffa25f98502a6dd99b547470721c9
bikramia <bikram.rhce@gmail.com>
2024-09-09 22:15:54 -0400

    Utility types

---
+++ Section5-Generics/utilitytype.ts
@@ -0,0 +1,20 @@
+interface Product { 
+    id:number; 
+    name:string; 
+    price:number; 
+}
+
+// // A Product where all properties are optional 
+// let product: Partial<Product>;
+
+// // A Product where all properties are required
+// let product: Required<Product>;
+
+// // A Product where all properties are read-only
+// letproduct: Readonly<Product>;
+
+// // A Product with two properties only (id and price)
+// let product: Pick<Product, 'id'|'price'>;
+
+// // A Product without a name 
+// let product: Omit<Product, 'name'>;
