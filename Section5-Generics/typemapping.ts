245f0083cc1262803db96c2e3798b510a9447f96
bikramia <bikram.rhce@gmail.com>
2024-09-09 22:11:43 -0400

    type mapping

---
+++ Section5-Generics/typemapping.ts
@@ -0,0 +1,11 @@
+type ReadOnly<T> = {
+    readonly [K in keyof T]: T[K];
+};
+
+type Optional<T> = {  
+    [K in keyof T]?: T[K];
+};
+
+type Nullable<T> = {  
+    [K in keyof T]: T[K] | null;
+};
