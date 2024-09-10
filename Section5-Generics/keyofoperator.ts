37e156b674f8c3befd7b63089c443931b98af796
bikramia <bikram.rhce@gmail.com>
2024-09-09 22:07:36 -0400

    Keyof operator

---
+++ Section5-Generics/keyofoperator.ts
@@ -0,0 +1,12 @@
+// interface Product { 
+//     name:string;
+//     price:number;
+// }
+
+// let property: keyof Product;
+
+// // Same as
+// let property:'name' | 'price';
+// property = 'name'; 
+// property = 'price';
+// property = 'otherValue'; // Invalid The keyof operator
