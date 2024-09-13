e54e9133079f21916a1db3a480e5d5424b6fef04
bikramia <bikram.rhce@gmail.com>
2024-09-12 21:35:52 -0400

    Parameter decorators

--- Section6-Decorators/dist/incdex.js
+++ Section6-Decorators/dist/incdex.js
@@ -1,6 +1,22 @@
 "use strict";
-function Component(constructor) {
-    // Here we have a chance to modify members of 
-    // the target class. 
-    constructor.prototype.uniqueId = Date.now();
+// function Component(constructor: Function) { 
+//     // console.log('Component decorator called');
+//     // constructor.prototype.uniqueId = Date.now();
+//     // constructor.prototype.insertInDOM = () => {
+//     //     console.log('Inserting the component in the DOM');
+//     // }
+//     // Here we have a chance to modify members of 
+//     // the target class. 
+//     constructor.prototype.uniqueId = Date.now();
+// }
+// @Component
+// class ProfileComponent { 
+// }
+// Parameterized decorators
+function Component(value) {
+    return (constructor) => {
+        // Here we have a chance to modify members of
+        // the target class.
+        constructor.prototype.uniqueId = Date.now();
+    };
 }
