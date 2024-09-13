1be54a5a2ce77228ad5d00a8a3baaaa90d8e3ec2
bikramia <bikram.rhce@gmail.com>
2024-09-13 17:00:30 -0400

    Default Exports and Coexist with named exports

--- Section7-Modules/src/index.ts
+++ Section7-Modules/src/index.ts
@@ -1,4 +1,6 @@
-import { Circle, Square } from './shapes';
+import Store, { Format } from './storage'
 
-let circle = new Circle(1);
-console.log(circle.radius);
+// import { Circle, Square } from './shapes';
+
+// let circle = new Circle(1);
+// console.log(circle.radius);
