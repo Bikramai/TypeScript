6796dbeedbc426ba821a79264550e75a4c7abd9a
bikramia <bikram.rhce@gmail.com>
2024-09-28 08:19:44 -0400

    Calling the Backend by installing axios -reminder tsx

---
+++ Section9-React-with-TypeScript/reminders-app/src/services/reminder.tsx
@@ -0,0 +1,26 @@
+import axios from 'axios'
+import Reminder from '../models/reminder';
+
+
+class ReminderServices {
+    http = axios.create({
+        baseURL: 'https://jsonplaceholder.typicode.com'
+    });
+
+    async getReminders() {
+        const response = await this.http.get<Reminder[]>('/todo');
+        return response.data
+    }
+
+    async addReminders(title: string) {
+        const response = await this.http.post<Reminder[]>('/todo', { title });
+        return response.data
+    }
+
+    async removeReminder(id: number) {
+        const response = await this.http.delete('/todos/' + id);
+        return response.data;
+    }
+}
+
+export default new ReminderServices();
