b76778e8a9af1725f23a72585077ead241792867
bikramia <bikram.rhce@gmail.com>
2024-09-27 22:03:09 -0400

    Using the Stat Hook

--- Section9-React-with-TypeScript/reminders-app/src/App.tsx
+++ Section9-React-with-TypeScript/reminders-app/src/App.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import ReminderList from './components/ReminderList';
-import Reminder from './models/reminder';
-
-const reminders: Reminder[] = [
-  { id:1, title: 'Reminder 1' }
-]
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ReminderList from "./components/ReminderList";
+import Reminder from "./models/reminder";
 
 
 function App() {
+  const [reminders, setReminders] = useState<Reminder[]>([
+    { id: 1, title: "Reminder 1" },
+  ]);
+
   return (
     <div className="App">
-      <ReminderList  items={reminders}/>
+      <ReminderList items={reminders} />
     </div>
   );
 }
